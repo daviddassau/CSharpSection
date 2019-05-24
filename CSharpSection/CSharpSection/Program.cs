19dd2c6edb43a4750358ded0318a421ea748b7a1
David Dassau <davidtdassau@gmail.com>
2019-05-24 18:58:28 -0500

    Variables and scope

--- CSharpSection/CSharpSection/Program.cs
+++ CSharpSection/CSharpSection/Program.cs
@@ -8,18 +8,22 @@ namespace CSharpSection
 {
     class Program
     {
+        static int n1 = 432, n2 = 543;
         private static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-
-            Console.WriteLine(Add(432, 543));
+            
+            var result = Add(n1, n2);
 
             Console.ReadLine();
         }
 
         private static int Add(int n1, int n2)
         {
-            return n1 + n2;
+            int result;
+            result = n1 + n2;
+
+            return result;
         }
     }
 }
