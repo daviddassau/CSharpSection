fcd204b72f120d7d37567f7bd8828d1d9bf6ef17
David Dassau <davidtdassau@gmail.com>
2019-05-25 07:32:52 -0500

    Methods and variables challenge

--- CSharpSection/CSharpSection/Program.cs
+++ CSharpSection/CSharpSection/Program.cs
@@ -8,22 +8,47 @@ namespace CSharpSection
 {
     class Program
     {
-        static int n1 = 432, n2 = 543;
+        static float n1 = 432, n2 = 543;
         private static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            
-            var result = Add(n1, n2);
+            var addition = Add();
+            var subtraction = Subtract();
+            var multiply = Multiply();
+            var division = Divide();
+
+            Console.WriteLine(addition);
+            Console.WriteLine(subtraction);
+            Console.WriteLine(multiply);
+            Console.WriteLine(division);
 
             Console.ReadLine();
         }
 
-        private static int Add(int n1, int n2)
+        // Add 2 values
+        private static float Add()
         {
-            int result;
+            float result;
             result = n1 + n2;
 
             return result;
+        }
+
+        // Subtract 2 values
+        private static float Subtract()
+        {
+            return n1 - n2;
+        }
+
+        // Multiply 2 values
+        private static float Multiply()
+        {
+            return n1 * n2;
+        }
+
+        // Divide 2 values
+        private static float Divide()
+        {
+            return n1 / n2;
         }
     }
 }
