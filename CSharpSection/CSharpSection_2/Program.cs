be7e07a49be25acd0724df9f479204a384434711
David Dassau <davidtdassau@gmail.com>
2019-05-25 11:29:49 -0500

    Classes and namespaces

--- CSharpSection/CSharpSection_2/Program.cs
+++ CSharpSection/CSharpSection_2/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CSharpSection_2.Classes;
 
 namespace CSharpSection_2
 {
@@ -10,6 +11,34 @@ namespace CSharpSection_2
     {
         static void Main(string[] args)
         {
+            Console.WriteLine(SimpleMath.Divide(432.23f, 54523.2f));
+
+            
+            
+            Console.ReadLine();
+        }
+    }
+
+    class SimpleMath
+    {
+        public static float Add(float n1, float n2)
+        {
+            return n1 + n2;
+        }
+
+        public static float Subtract(float n1, float n2)
+        {
+            return n1 - n2;
+        }
+
+        public static float Multiply(float n1, float n2)
+        {
+            return n1 * n2;
+        }
+
+        public static float Divide(float n1, float n2)
+        {
+            return n1 / n2;
         }
     }
 }
